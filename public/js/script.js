7dd34977217648c3b72ea93707435df02d34b61c
Cozy-Habit <110344825+Cozy-Habit@users.noreply.github.com>
2022-12-17 22:00:33 +0100

    Fixing stuff about hamburger menu

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 
+
 navToggle.addEventListener("click", () => {
     const visibility = nav.getAttribute('data-visible');
 
